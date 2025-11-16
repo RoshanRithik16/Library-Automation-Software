8a08bdbc0d0959f1376b177a75497b9875592fc4
RoshanRithik16 <roshan162004@gmail.com>
2025-11-16 14:05:45 +0530

    Delete linking.py

--- linking.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://rsb:dbpass@rsbcluster.pf0zwp8.mongodb.net/")
-db = client['Book']
-collection=db['BookData']
-print(collection.find_one())
